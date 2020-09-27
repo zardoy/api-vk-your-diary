2a72aff44be063d4dd7e7ab5f5932a068321e970
Vitaly Turovsky <vital2580@icloud.com>
2020-09-27 13:50:01 +0300

    refactor: simplify auth logic

--- api/app.ts
+++ api/app.ts
@@ -36,17 +36,17 @@ const pickVkParams = <K extends string = string>(vk_params: VK_params, pick_para
 
 
 schema.addToContext(({ req }) => {
-    //todo err
-    if (process.env.VK_SECRET_KEY === undefined) throw new TypeError(`Environment variable VK_SECRET_KEY is not provided.`);
-
     //todo not safe
-    if (process.env.NEXUS_STAGE === "test" && process.env.TEST_USER_ID) {
+    if (process.env.NEXUS_STAGE === "test" || process.env.NEXUS_STAGE === "dev") {
         return {
             vk_params: {
-                user_id: process.env.TEST_USER_ID
+                user_id: process.env.TEST_USER_ID || "35039"
             }
         };
     };
+
+    //todo err
+    if (process.env.VK_SECRET_KEY === undefined) throw new TypeError(`Environment variable VK_SECRET_KEY is not provided.`);
 
     const vkParams = new URLSearchParams(req.headers.authorization);
 
@@ -71,7 +71,6 @@ schema.addToContext(({ req }) => {
     //todo any is evil
     let vk_launch_params: VK_params = _.fromPairs(Array.from(vkParams as any));
     const ctx_vk_params = pickVkParams(vk_launch_params, ["user_id", "app_id", "platform"]);
-    ctx_vk_params.user_id = "35039";
     if (!isFinite(+ctx_vk_params.user_id)) throw new TypeError(`user_id param is not a number: ${ctx_vk_params.user_id}`);
 
     return {
